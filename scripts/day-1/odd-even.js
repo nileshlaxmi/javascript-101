617126015756f5294d325291d2deb4daded1b533
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2025-06-30 07:32:03 +0530

    30-06 odd-even

---
+++ scripts/day-1/odd-even.js
@@ -0,0 +1,15 @@
+// 30-6-25
+
+// Program to Check if a Number is Odd or Even
+const checkOddEven = num => {
+    return num % 2 == 0 ? "Even" : "Odd"
+}
+
+console.log(checkOddEven(10))
+console.log(checkOddEven(5))
+console.log(checkOddEven(6))
+console.log(checkOddEven(9))
+console.log(checkOddEven(124))
+console.log(checkOddEven(567))
+console.log(checkOddEven(100))
+console.log(checkOddEven(55))
