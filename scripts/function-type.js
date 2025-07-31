0deb407bca21cdc2fa2d93db89b1f325123883ea
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2025-07-31 22:25:47 +0530

    JS101 functionType added

---
+++ scripts/function-type.js
@@ -0,0 +1,15 @@
+// WAP to Check If a Variable is of Function Type
+
+const a = () => {}
+
+const functionType = (n) => {
+    console.log(`Input is ${typeof n} type`)
+};
+
+functionType(8);
+functionType(a);
+functionType(6);
+functionType(9);
+functionType(124);
+functionType('a');
+
