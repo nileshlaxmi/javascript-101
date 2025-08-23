a04a10aba720ef0d0835b380b1c3edea0c0b19da
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2025-08-23 23:00:07 +0530

    JS101 last digits updated

--- scripts/last-digits.js
+++ scripts/last-digits.js
@@ -1,20 +1,17 @@
 /* program to check whether the last digit of three
 numbers is same */
 
-// take input
-const a = prompt('Enter a first integer: ');
-const b = prompt('Enter a second integer: ');
-const c = prompt('Enter a third integer: ');
+const printLastDigits = (a, b, c) => {
+    const result1 = a % 10;
+    const result2 = b % 10;
+    const result3 = c % 10;
 
-// find the last digit
-const result1 = a % 10;
-const result2 = b % 10;
-const result3 = c % 10;
+    if (result1 == result2 && result1 == result3) {
+        console.log(`${a}, ${b} and ${c} have the same last digit.`);
+    }
+    else {
+        console.log(`${a}, ${b} and ${c} have different last digit.`);
+    }
+}
 
-// compare the last digits
-if(result1 == result2 && result1 == result3) {
-    console.log(`${a}, ${b} and ${c} have the same last digit.`);
-}
-else {
-    console.log(`${a}, ${b} and ${c} have different last digit.`);
-}
+printLastDigits(34, 54, 74);
